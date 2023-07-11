fc59f980548e501b3c8f223a75a31513b16c18f9
Adriano <adriano_alves_2008@hotmail.com>
2023-07-11 15:08:57 -0300

    criando header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import AuthProvider from '@/components/AuthProvider'
 import './globals.css'
 import { Poppins } from 'next/font/google'
+import Header from '@/components/Header'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -16,7 +17,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={poppins.className}>{children}</body>
+        <body className={poppins.className}>
+          <Header />
+          {children}
+        </body>
       </AuthProvider>
     </html>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import Button from './Button'
+
+export default function Header() {
+  return (
+    <div className="flex gap-4 justify-center p-2 flex-wrap">
+      <Link href="/">Home</Link>
+      <Link href="/sobre">Sobre</Link>
+      <Link href="/contato">Contato</Link>
+      <Button
+        text="Sair"
+        className="bg-red-600 text-white rounded px-2 cursor-pointer"
+      />
+    </div>
+  )
+}
