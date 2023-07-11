a3726a7c2cc61e856e3bd2416c524ce867f6bf68
Adriano <adriano_alves_2008@hotmail.com>
2023-07-11 14:58:52 -0300

    Criando o Auth Provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import AuthProvider from '@/components/AuthProvider'
 import './globals.css'
 import { Poppins } from 'next/font/google'
 
@@ -14,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <AuthProvider>
+        <body className={poppins.className}>{children}</body>
+      </AuthProvider>
     </html>
   )
 }
